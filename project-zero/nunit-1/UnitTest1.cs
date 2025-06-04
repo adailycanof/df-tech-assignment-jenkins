6cc1e4172a1d5c1e4190b54bfb791b119f6e56c6
Simon Hill <simon.hill@adailycanof.co.uk>
2025-06-04 20:43:51 +0100

    upload stuff

---
+++ project-zero/nunit-1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace NUnit1Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass("This test always passes.");
+        }
+    }
+}
